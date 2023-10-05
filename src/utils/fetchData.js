f40b05aa064c044dcd880507c595d355d77daa49
vishal bhadoriya <vishalbhadoriya9765@gmail.com>
2023-10-05 15:31:37 +0530

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key': '744015c0e4msh83e21048ff746dep1f4eeejsnc3adba41a819'
+    },
+  };
+  
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': '744015c0e4msh83e21048ff746dep1f4eeejsnc3adba41a819'
+    },
+  };
+  
+  export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
+    return data;
+  };
